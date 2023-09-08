8e3d42391dad6323b24d6771622d216c1b5c9e99
Gabriele Sarti <gabriele.sarti996@gmail.com>
2023-09-08 21:11:41 +0200

    CTI scripts

---
+++ scripts/run_scat_cti.sh
@@ -0,0 +1,73 @@
+#python scripts/tag_cti_metrics.py \
+#    --examples_path outputs/processed_examples/scat-marian-small-scat.tsv \
+#    --model_name context-mt/scat-marian-small-ctx4-cwd1-en-fr \
+#    --model_type marian-small
+#
+#
+#python scripts/tag_cti_metrics.py \
+#    --examples_path outputs/processed_examples/scat-marian-small-scat.tsv \
+#    --model_name context-mt/scat-marian-small-ctx4-cwd1-en-fr \
+#    --model_type marian-small
+#
+#
+#python scripts/tag_cti_metrics.py \
+#    --examples_path outputs/processed_examples/scat-marian-big-scat.tsv \
+#    --model_name context-mt/scat-marian-big-ctx4-cwd1-en-fr \
+#    --model_type marian-big
+# 
+# 
+#python scripts/tag_cti_metrics.py \
+#    --examples_path outputs/processed_examples/scat-marian-big-scat.tsv \
+#    --model_name context-mt/scat-marian-big-ctx4-cwd1-en-fr \
+#    --model_type marian-big
+# 
+# 
+# python scripts/tag_cti_metrics.py \
+#    --examples_path outputs/processed_examples/scat-mbart50-1toM-scat.tsv \
+#    --model_name context-mt/scat-mbart50-1toM-ctx4-cwd1-en-fr \
+#    --model_type mbart50-1toM
+#
+#
+#python scripts/tag_cti_metrics.py \
+#    --examples_path outputs/processed_examples/scat-mbart50-1toM-scat.tsv \
+#    --model_name context-mt/scat-mbart50-1toM-ctx4-cwd1-en-fr \
+#    --model_type mbart50-1toM
+
+
+# Target context models
+
+
+python scripts/tag_cti_metrics.py \
+    --examples_path outputs/processed_examples/scat-marian-small-scat-target.tsv \
+    --model_name context-mt/scat-marian-small-target-ctx4-cwd0-en-fr \
+    --model_type marian-small
+
+
+python scripts/tag_cti_metrics.py \
+    --examples_path outputs/processed_examples/scat-marian-small-scat-target.tsv \
+    --model_name context-mt/scat-marian-small-target-ctx4-cwd0-en-fr \
+    --model_type marian-small
+
+
+python scripts/tag_cti_metrics.py \
+    --examples_path outputs/processed_examples/scat-marian-big-scat-target.tsv \
+    --model_name context-mt/scat-marian-big-target-ctx4-cwd0-en-fr \
+    --model_type marian-big
+
+
+python scripts/tag_cti_metrics.py \
+    --examples_path outputs/processed_examples/scat-marian-big-scat-target.tsv \
+    --model_name context-mt/scat-marian-big-target-ctx4-cwd0-en-fr \
+    --model_type marian-big
+
+
+python scripts/tag_cti_metrics.py \
+    --examples_path outputs/processed_examples/scat-mbart50-1toM-scat-target.tsv \
+    --model_name context-mt/scat-mbart50-1toM-target-ctx4-cwd0-en-fr \
+    --model_type mbart50-1toM
+
+
+python scripts/tag_cti_metrics.py \
+    --examples_path outputs/processed_examples/scat-mbart50-1toM-scat-target.tsv \
+    --model_name context-mt/scat-mbart50-1toM-target-ctx4-cwd0-en-fr \
+    --model_type mbart50-1toM
