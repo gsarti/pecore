b897ad0742a87cdfbc66751cec5d590690063764
Gabriele Sarti <gabriele.sarti996@gmail.com>
2023-09-10 11:16:00 +0200

    Fix marian small disc_eval_mt cti scores

--- scripts/run_cti_eval_and_preds.sh
+++ scripts/run_cti_eval_and_preds.sh
@@ -2,37 +2,41 @@ for model in marian-small marian-big mbart50-1toM;
 do
     
     python scripts/evaluate_tagged_metrics.py \
-        --scores_path outputs/scores/scat-${model}-scat-cti.tsv \
+        --scores_path outputs/scores/scat/scat-${model}-scat-cti.tsv \
         --eval_mode cti \
         --average_example_scores \
         --metrics random pcxmi kl_divergence \
         --save_preds \
-        --output_dir outputs/metrics_evals/scat
+        --output_dir outputs/metrics_evals/scat \
+        --dataset scat
     
     python scripts/evaluate_tagged_metrics.py \
-        --scores_path outputs/scores/scat-${model}-scat-target-cti.tsv \
+        --scores_path outputs/scores/scat/scat-${model}-scat-target-cti.tsv \
         --eval_mode cti \
         --average_example_scores \
         --metrics random pcxmi kl_divergence \
         --save_preds \
-        --output_dir outputs/metrics_evals/scat
+        --output_dir outputs/metrics_evals/scat \
+        --dataset scat
     
     for split in anaphora lexical-choice;
     do
         python scripts/evaluate_tagged_metrics.py \
-            --scores_path outputs/scores/disc_eval_mt-${split}-${model}-scat-cti.tsv \
+            --scores_path outputs/scores/disc_eval_mt/${split}/disc_eval_mt-${split}-${model}-scat-cti.tsv \
             --eval_mode cti \
             --average_example_scores \
             --metrics random pcxmi kl_divergence \
             --save_preds \
-            --output_dir outputs/metrics_evals/disc_eval_mt/${split}
+            --output_dir outputs/metrics_evals/disc_eval_mt/${split} \
+            --dataset disc_eval_mt
         
         python scripts/evaluate_tagged_metrics.py \
-            --scores_path outputs/scores/disc_eval_mt-${split}-${model}-scat-target-cti.tsv \
+            --scores_path outputs/scores/disc_eval_mt/${split}/disc_eval_mt-${split}-${model}-scat-target-cti.tsv \
             --eval_mode cti \
             --average_example_scores \
             --metrics random pcxmi kl_divergence \
             --save_preds \
-            --output_dir outputs/metrics_evals/disc_eval_mt/${split}
+            --output_dir outputs/metrics_evals/disc_eval_mt/${split} \
+            --dataset disc_eval_mt
     done
 done
